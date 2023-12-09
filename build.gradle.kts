089e63cdbb16153eca7993cecb2946beb5674905
vitortcmiranda <vitortcmout@gmail.com>
2023-12-09 11:35:29 -0300

    feat(Flyway): adding flyway to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	implementation("org.flywaydb:flyway-core:8.5.11")
 }
 
 tasks.withType<KotlinCompile> {
